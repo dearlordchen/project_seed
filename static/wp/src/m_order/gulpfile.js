70431869b4e8150dbe7efe897660b0df9e81e672
Tony Luo <Hengjie@LUOs-MacBook-Pro.local>
2016-02-29 14:28:03 +0800

    添加统一配置

--- static/wp/src/m_order/gulpfile.js
+++ static/wp/src/m_order/gulpfile.js
@@ -5,40 +5,43 @@ var path = require('path')
 var webpackDevConf = require('../../config/webpack.dev.conf.js')
 var webpackProdConf = require('../../config/webpack.prod.conf.js')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var sftp = require('gulp-sftp')
+var myConfig = require('../../config/my-config.json')
 
 var viewName = 'm_order'
 
-// gulp.task('webpack-dev', function (cb) {
-//   var myConfig = Object.create(webpackDevConf)
-//
-//   myConfig.entry.app = path.resolve(__dirname, './main.js')
-//
-//   myConfig.plugins = (config.plugins || []).concat([
-//     new HtmlWebpackPlugin({
-//       filename: '../index.html',
-//       template: path.resolve(__dirname, './index.html'),
-//       chunks: ['app']
-//     })
-//   ])
-//
-//   webpack(myConfig, function (err, stats) {
-//     if (err) throw new gutil.PluginError('build', err)
-//     gutil.log('[build]', stats.toString({
-//       colors: true
-//     }))
-//     cb()
-//   })
-// })
+// 上传到dev服务器
+gulp.task('upload-dev', ['webpack-dev'], function () {
+  return gulp.src('../../dist/' + viewName + '/**')
+    .pipe(sftp({
+			host: myConfig.sftp.dev.host,
+      port: myConfig.sftp.dev.port,
+			user: myConfig.sftp.dev.user,
+			pass: myConfig.sftp.dev.pass,
+      remotePath: myConfig.sftp.dev.staticRemotePath + viewName
+		}))
+})
+gulp.task('upload-ssi-dev', ['upload-dev'], function () {
+  return gulp.src('../../../sinclude/wp/' + viewName + '/**')
+  .pipe(sftp({
+    host: myConfig.sftp.dev.host,
+    port: myConfig.sftp.dev.port,
+    user: myConfig.sftp.dev.user,
+    pass: myConfig.sftp.dev.pass,
+    remotePath: myConfig.sftp.dev.ssiRemotePath + viewName
+  }))
+})
 
-gulp.task('webpack-prod', function (cb) {
-  var myConfig = Object.create(webpackProdConf)
+// 打包压缩
+gulp.task('webpack-dev', function (cb) {
+  var webpackConf = Object.create(webpackProdConf)
 
-  myConfig.entry[viewName] = path.resolve(__dirname, './main.js')
+  webpackConf.entry[viewName] = path.resolve(__dirname, './main.js')
 
-  myConfig.output.path += '/' + viewName + '/'
-  myConfig.output.publicPath += '/' + viewName + '/'
+  webpackConf.output.path += '/' + viewName + '/'
+  webpackConf.output.publicPath += '/' + viewName + '/'
 
-  myConfig.plugins = (myConfig.plugins || []).concat([
+  webpackConf.plugins = (webpackConf.plugins || []).concat([
     new HtmlWebpackPlugin({
       filename: 'html/index.html',
       template: path.resolve(__dirname, './index.html'),
@@ -56,7 +59,7 @@ gulp.task('webpack-prod', function (cb) {
     })
   ])
 
-  webpack(myConfig, function (err, stats) {
+  webpack(webpackConf, function (err, stats) {
     if (err) throw new gutil.PluginError('build', err)
     gutil.log('[build]', stats.toString({
       colors: true
@@ -65,6 +68,41 @@ gulp.task('webpack-prod', function (cb) {
   })
 })
 
-gulp.task('build', function () {
-  gulp.start('webpack-prod')
+gulp.task('webpack-prod', function (cb) {
+  var webpackConf = Object.create(webpackProdConf)
+
+  webpackConf.entry[viewName] = path.resolve(__dirname, './main.js')
+
+  webpackConf.output.path += '/' + viewName + '/'
+  webpackConf.output.publicPath += '/' + viewName + '/'
+
+  webpackConf.plugins = (webpackConf.plugins || []).concat([
+    new HtmlWebpackPlugin({
+      filename: 'html/index.html',
+      template: path.resolve(__dirname, './index.html'),
+      chunks: [viewName]
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../../sinclude/wp/' + viewName + '/cssi.html',
+      template: path.resolve(__dirname, './cssi.html'),
+      chunks: [viewName]
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../../sinclude/wp/' + viewName + '/jsi.html',
+      template: path.resolve(__dirname, './jsi.html'),
+      chunks: [viewName]
+    })
+  ])
+
+  webpack(webpackConf, function (err, stats) {
+    if (err) throw new gutil.PluginError('build', err)
+    gutil.log('[build]', stats.toString({
+      colors: true
+    }))
+    cb()
+  })
+})
+
+gulp.task('default', ['upload-ssi-dev'], function () {
+  gulp.watch('./**', ['upload-ssi-dev'])
 })
